65ae6589e3495dcacfb4c5916fa183c78ef8d71a
jspc <james@zero-internet.org.uk>
2016-09-16 10:15:53 +0100

    Split out code base to logical files

---
+++ api.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    "net/http"
+    "log"
+    "fmt"
+    "encoding/json"
+)
+
+func BuildHealthcheck(w http.ResponseWriter, r *http.Request){
+    LogRequest(r)
+
+    var metrics []Metric
+    metrics = append(metrics, LoadAvg())
+    metrics = append(metrics, Memory())
+    metrics = append(metrics, DiskUsage())
+
+    healthcheck.Checks = metrics
+
+    j, err := json.Marshal(healthcheck)
+    if err != nil {
+        log.Fatalf("Could not marshal projects data: %s\n", err)
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    fmt.Fprintf(w, string(j))
+}
+
+func LogRequest(r *http.Request) {
+    log.Printf( "%s :: %s %s",
+        r.RemoteAddr,
+        r.Method,
+        r.URL.Path)
+}
